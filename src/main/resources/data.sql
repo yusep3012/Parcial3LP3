4c6488b26a961de54591778fcc4840373c2f8b36
Yusep Ruiz <yusep3012@hotmail.com>
2020-11-17 21:59:37 -0500

    Cambié nombres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into estudiante (cedula,nombre,apellido,promedio)
-values  (000,'SI','Ruiz',4.0),
-        (111,'Andres', 'El patrón',2.0),
-        (222,'SI', 'Munera',5.0),
+INSERT INTO estudiante (cedula,nombre,apellido,promedio)
+VALUES  (000,'Yusep','Ruiz',4.0),
+        (111,'Andres', 'El patrón',4.5),
+        (222,'Luis', 'Munera',5.0),
         (333,'Pepito','Perez',1.0),
-        (444,'SI','aaa',3.6),
-        (555,'SI', 'Munera',5.0),
-        (666,'SI', 'El patrón',4.0),
-        (777,'ddd','Perez',1.0);
+        (444,'Romario','Mamario',3.6),
+        (555,'Marcela', 'Agachate y Conocela',5.0),
+        (666,'Rodrigo', 'Si se agacha le digo',2.0),
+        (777,'Sebastián','Alarcón',4.8);
